ffcf695dc808c5e9a150322ca884b32bb87b3442
Zia <zeazzzz4@gmail.com>
2021-12-18 12:23:13 +0530

    the navbar design is created

--- 01-starting-project/src/components/Navbar.js
+++ 01-starting-project/src/components/Navbar.js
@@ -1,6 +1,24 @@
 import React from "react";
-
+import "./Navbar.css";
 const Navbar = () => {
-  return <p> this is supposed to be the navbar</p>;
+  return (
+    <div className="navbar">
+      <h2>ShoppersDestination</h2>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+        <li>
+          <a>Login/SignUp</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Navbar;
